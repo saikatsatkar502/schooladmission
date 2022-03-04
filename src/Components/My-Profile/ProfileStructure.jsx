9d79df6d45fe27a1cde2c88d9a28fd115a97fa05
soumik sarkar <saikatsarkar502@gmail.com>
2022-03-04 22:57:33 +0530

    basic structure created

---
+++ src/Components/My-Profile/ProfileStructure.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function ProfileStructure() {
+
+    const BackGroundPicture = ()=>{
+        
+    }
+
+    return (
+        <div className='ProfileTop'>
+            <div id='BackGroundPicture'>
+                
+            </div>
+            <div id='profile'>
+
+            </div>
+            
+
+        </div>
+    )
+}
